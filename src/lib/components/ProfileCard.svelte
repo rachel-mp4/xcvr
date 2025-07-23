30737624148f68c4a6851d34239d317d9043863e
rachel-mp4 <rachel@moth11.net>
2025-07-23 13:28:43 -0400

    add color to profileCard

--- src/lib/components/ProfileCard.svelte
+++ src/lib/components/ProfileCard.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
   import type { ProfileView } from "$lib/types";
+  import { hexToContrast, hexToTransparent, numToHex } from "$lib/colors";
   interface Props {
     profile: ProfileView;
   }
 
   let { profile }: Props = $props();
+  let bg = $derived(numToHex(profile.color ?? 0));
+  let bl = $derived(hexToTransparent(bg));
+  let fg = $derived(hexToContrast(bg));
+  let fl = $derived(hexToTransparent(fg));
 </script>
 
-<div class="profile">
+<div class="profile" style="--bg: {bg}; --bl: {bl}; --fg: {fg}; --fl: {fl}">
   {#if profile.displayName}<h3>{profile.displayName}</h3>{/if}
   <a href={`/a/${profile.handle}`}>@{profile.handle}</a>
   {#if profile.status}<div>{profile.status}</div>{/if}
 </div>
+
+<style>
+  .profile {
+    background: var(--bg);
+  }
+</style>
