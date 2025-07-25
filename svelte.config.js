ed286c26814e8f4d367f3783cfc3d12edca0bf85
rachel-mp4 <rachel@moth11.net>
2025-07-25 15:46:09 -0400

    add trailing slash

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,8 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: 'index.html'
-		}
-		)
+		}),
+		trailingSlash: 'always'
 	}
 };
 
