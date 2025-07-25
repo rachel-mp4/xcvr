e7941e97c88bd4d7cfc93075e40998429df653b7
rachel-mp4 <rachel@moth11.net>
2025-07-25 10:54:41 -0400

    maybe silly

--- src/routes/c/[handle]/[rkey]/history/+page.svelte
+++ src/routes/c/[handle]/[rkey]/history/+page.svelte
@@ -3,13 +3,23 @@
   import History from "$lib/components/History.svelte";
   let { data }: PageProps = $props();
   let messages = $state(data.messages);
+  $effect(() => {
+    if (data.messages) {
+      messages = data.messages;
+    }
+  });
   let nextCursor = $state(data.cursor);
-  let hasMore = $state(!!data.cursor);
+  $effect(() => {
+    if (data.cursor) {
+      messages = data.cursor;
+    }
+  });
+  let hasMore = $derived(nextCursor);
   let loading = $state(false);
   const base = import.meta.env.VITE_API_URL;
   const endpoint = "/xrpc/org.xcvr.lrc.getMessages";
-  const query = `?channelURI=${data.uri}&cursor=${nextCursor}`;
-  console.log(`${base}${endpoint}${query}`);
+  let query = $derived(`?channelURI=${data.uri}&cursor=${nextCursor}`);
+  $effect(() => console.log(`${base}${endpoint}${query}`));
   const loadMore = async () => {
     if (loading || !hasMore || !nextCursor) return;
     loading = true;
@@ -21,7 +31,6 @@
       const newData = await res.json();
       messages = [...newData.messages, ...messages];
       nextCursor = newData.cursor;
-      hasMore = !!newData.cursor;
     } catch (e) {
       console.error("failed to load more!", e);
     } finally {
