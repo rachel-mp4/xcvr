04567f7dbca0cec018360cd18da600527be125e4
rachel-mp4 <rachel@moth11.net>
2025-07-25 10:47:53 -0400

    history get uri bc it involves did and not handle

--- src/routes/c/[handle]/[rkey]/history/+page.ts
+++ src/routes/c/[handle]/[rkey]/history/+page.ts
@@ -2,16 +2,23 @@ import type { PageLoad } from './$types'
 
 export const load: PageLoad = async ({ params, fetch }) => {
   const base = import.meta.env.VITE_API_URL
+  const endpoint1 = "/xrpc/org.xcvr.actor.resolveChannel"
+  const query1 = `?handle=${params.handle}&rkey=${params.rkey}`
+  const url1 = `${base}${endpoint1}${query1}`
+  const res1 = await fetch(url1)
+  if (!res1.ok) {
+    return
+  }
+
+  const response1 = await res1.json()
+  const uri = response1.uri
   const endpoint = "/xrpc/org.xcvr.lrc.getMessages"
-  const uri = `at://${params.handle}/org.xcvr.feed.channel/${params.rkey}`
   const query = `?channelURI=${uri}`
   const url = `${base}${endpoint}${query}`
   const res = await fetch(url)
 
   if (!res.ok) {
-    return {
-      messages: ""
-    }
+    return
   }
 
   const response = await res.json()
