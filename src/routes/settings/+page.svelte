4e35ed20d64be7335e4e5f466303dd50fa442983
rachel-mp4 <rachel@moth11.net>
2025-07-26 15:20:07 -0400

    add logout button

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -3,4 +3,7 @@
     there are no settings yet. if you need something, please let me know!
     rachel@moth11.net or @moth11.net
   </p>
+  <form action="{import.meta.env.VITE_API_URL}/oauth/logout" method="POST">
+    <input type="submit" value="log out" />
+  </form>
 </main>
