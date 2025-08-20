fdd32e88471b12398d99fe6e822f74882c3a4042
rachel-mp4 <rachel@moth11.net>
2025-08-20 13:38:27 -0400

    i think this should fix bad logins

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,13 +3,13 @@ import type { LayoutLoad } from "./$types"
 export const load: LayoutLoad = async ({ fetch }) => {
 	const results = await Promise.allSettled([
 		fetch(`${import.meta.env.VITE_API_URL}/xrpc/org.xcvr.feed.getChannels`)
-			.then(r => r.json()),
+			.then(r => r.ok ? r.json() : []),
 		fetch(`${import.meta.env.VITE_API_URL}/oauth/whoami`)
-			.then(r => r.json())
+			.then(r => r.ok ? r.json() : null)
 	])
 	return {
 		channels: results[0].status === 'fulfilled' ? results[0].value : [],
-		myProfile: results[1].status === 'fulfilled' ? { ...(results[1].value), loggedIn: true } : {
+		myProfile: results[1].status === 'fulfilled' && results[1].value !== null ? { ...(results[1].value), loggedIn: true } : {
 			handle: "xcvr.org",
 			defaultNick: "wanderer",
 			color: Math.floor(Math.random() * 16777216),
