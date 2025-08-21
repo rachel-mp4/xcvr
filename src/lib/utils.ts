d684ef3f0e62f7723c4f120ea10993b97d822009
rachel-mp4 <rachel@moth11.net>
2025-08-20 22:54:29 -0400

    not sure

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -86,21 +86,23 @@ export function sanitizeHandle(input: string) {
 
 export function smartAbsoluteTimestamp(then: number): string {
   const now = Date.now()
-  if (now - then < 1000 * 60 * 60 * 18) {
+  if (then > now) {
+    return "in the future"
+  } else if (now - then < 1000 * 60 * 60 * 18) {
     const formatter = new Intl.DateTimeFormat("en-us", { hour: "numeric", minute: "numeric" })
-    return formatter.format(then)
+    return formatter.format(then).toLocaleLowerCase()
   } else if (now - then < 1000 * 60 * 60 * 24 * 6) {
     const formatter = new Intl.DateTimeFormat("en-us", { weekday: "long", dayPeriod: "long" })
-    return formatter.format(then)
+    return formatter.format(then).toLocaleLowerCase()
   } else if (now - then < 1000 * 60 * 60 * 24 * 333) {
     const formatter1 = new Intl.DateTimeFormat("en-us", { weekday: "long" })
     const formatter2 = new Intl.DateTimeFormat("en-us", { month: "long", dayPeriod: "long" })
-    return `a ${formatter1.format(then)} in ${formatter2.format(then)}`
+    return `a ${formatter1.format(then)} in ${formatter2.format(then)}`.toLocaleLowerCase()
   } else {
     const formatter1 = new Intl.DateTimeFormat("en-us", { weekday: "long" })
     const formatter2 = new Intl.DateTimeFormat("en-us", { month: "long" })
     const formatter3 = new Intl.DateTimeFormat("en-us", { year: "numeric", dayPeriod: "long" })
-    return `a ${formatter1.format(then)} in ${formatter2.format(then)} ${formatter3.format(then)}`
+    return `a ${formatter1.format(then)} in ${formatter2.format(then)} ${formatter3.format(then)}`.toLocaleLowerCase()
   }
 }
 
