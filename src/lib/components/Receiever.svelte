753baa88a28b6f416eefdfe3b43008679d82bda0
rachel-mp4 <rachel@moth11.net>
2025-09-05 16:41:34 -0400

    idk

--- src/lib/components/Receiever.svelte
+++ src/lib/components/Receiever.svelte
@@ -8,24 +8,21 @@
     onunmute?: (id: number) => void;
   }
   let { messages, mylocaltext, onmute, onunmute }: Props = $props();
-  const getFontSizeFor = (index: number): string => {
-    const last = messages.length - 1;
-    const diff = index - last;
-    const guess = 2 - diff * 0.012987;
-    const res = Math.min(Math.max(guess, 1), 2);
-    return `${res}rem`;
-  };
 </script>
 
 <div id="receiver">
   {#each messages as message, index}
+    {@const last = messages.length - 1}
+    {@const diff = index - last}
+    {@const guess = 2 - diff * 0.012987}
+    {@const res = Math.min(Math.max(guess, 1), 2)}
     <Transmission
       {message}
       mylocaltext={message.active && message.mine ? mylocaltext : undefined}
       margin={0}
       {onmute}
       {onunmute}
-      fs={getFontSizeFor(index)}
+      fs={`${res}rem`}
     />
   {/each}
 </div>
