a23c70a4d3140d5009e2b77f73e5a4f855281dc5
rachel-mp4 <rachel@moth11.net>
2025-09-05 15:05:57 -0400

    add log

--- src/lib/components/AutoGrowTextArea.svelte
+++ src/lib/components/AutoGrowTextArea.svelte
@@ -24,6 +24,7 @@
     let inputEl: HTMLTextAreaElement;
     function adjust(event: Event) {
         onInput?.(event as InputEvent);
+        console.log("next message is emoji check: ");
         console.log(checkEmoji(inputEl.selectionStart, inputEl.selectionEnd));
     }
 
