7437caf8f45c188ad0cf0f990043370a4b08bb5e
rachel-mp4 <rachel@moth11.net>
2025-10-12 21:39:56 -0400

    make prettier a lil smidge

---
+++ src/routes/b/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  const base = import.meta.env.VITE_API_URL;
+</script>
+
+<main>
+  <h1>try to ban someone</h1>
+  <p>this probably shouldn't do anything unless you're me</p>
+  <form action={`${base}/oauth/ban`} method="POST">
+    <div>
+      <label for="user">user to ban: </label>
+      <input type="text" name="user" id="user" required />
+    </div>
+    <div>
+      <label for="days">number of days to ban for: </label>
+      <input type="text" name="days" id="days" />
+    </div>
+    <div>
+      <label for="reason">reason: </label>
+      <input type="text" name="reason" id="reason" />
+    </div>
+    <div>
+      <input type="submit" value="ban" />
+    </div>
+  </form>
+</main>
--- src/routes/b/[id]/+page.svelte
+++ src/routes/b/[id]/+page.svelte
@@ -8,42 +8,48 @@
   const base = import.meta.env.VITE_API_URL;
 </script>
 
-{#if data.ban}
-  {#await fetchhandle(data.ban.did)}
-    fetching handle
-  {:then result}
-    {result.alsoKnownAs[0]}
-  {:catch}
-    error fetching handle
-  {/await}
-  ({data.ban.did}) was banned {smartAbsoluteTimestamp(data.ban.bannedAt)}
-  {#if data.ban.reason}
-    for {data.ban.reason}.
+<main>
+  {#if data.ban}
+    {#await fetchhandle(data.ban.did)}
+      fetching handle
+    {:then result}
+      <h1>{result.alsoKnownAs[0]}</h1>
+    {:catch}
+      error fetching handle
+    {/await}
+    ({data.ban.did}) was banned {smartAbsoluteTimestamp(
+      Date.parse(data.ban.bannedAt),
+    )}
+    {#if data.ban.reason}
+      for {data.ban.reason}.
+    {/if}
+    {#if data.ban.till}
+      the ban will end on {dumbAbsoluteTimestamp(data.ban.till)}.
+    {/if}
+  {:else}
+    i can't find ban
   {/if}
-  {#if data.ban.till}
-    the ban will end on {dumbAbsoluteTimestamp(data.ban.till)}.
-  {/if}
-{:else}
-  i can't find ban
-{/if}
+</main>
 
 {#if data.myProfile.loggedIn}
-  try to ban user
-  <form action={`${base}/oauth/ban`} method="POST">
-    <div>
-      <label for="user">user to ban: </label>
-      <input type="text" name="user" id="user" required />
-    </div>
-    <div>
-      <label for="days">number of days to ban for: </label>
-      <input type="text" name="days" id="days" />
-    </div>
-    <div>
-      <label for="reason">reason: </label>
-      <input type="text" name="reason" id="reason" />
-    </div>
-    <div>
-      <input type="submit" value="ban" />
-    </div>
-  </form>
+  <aside>
+    <h3>try to ban user</h3>
+    <form action={`${base}/oauth/ban`} method="POST">
+      <div>
+        <label for="user">user to ban: </label>
+        <input type="text" name="user" id="user" required />
+      </div>
+      <div>
+        <label for="days">number of days to ban for: </label>
+        <input type="text" name="days" id="days" />
+      </div>
+      <div>
+        <label for="reason">reason: </label>
+        <input type="text" name="reason" id="reason" />
+      </div>
+      <div>
+        <input type="submit" value="ban" />
+      </div>
+    </form>
+  </aside>
 {/if}
